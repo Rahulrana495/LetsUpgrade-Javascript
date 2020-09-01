301c0493fbfad719ce364fc8d3e893495c80f668
Rahulrana495 <70511727+Rahulrana495@users.noreply.github.com>
2020-09-01 08:07:08 +0530

    Add files via upload

---
+++ Day-2/index.js
@@ -0,0 +1,26 @@
+//Que-1
+let title = "JAVA"
+let charac = title.charAt(2)
+console.log(charac)
+//Que-2
+let mins= 2
+console.log( mins)
+let seconds = 60*mins 
+console.log(seconds)
+//Que-3
+let Friends=["Rahul","Lakshman","Birbal","Dev"]
+console.log(Friends[0])
+//Que-4
+let names=["DAvid","Rahul","Mohit","THomas"]
+console.log(names)
+for(let i = 0; i < names.length; i++){
+    if(names[i].includes("a")){
+        console.log(names[i])
+    }
+}
+//Que-5
+let data=[40,30,23,12,4]
+console.log(data)
+for (let i = 4; i>=0; i--) {
+       console.log(data[i]);
+     }
